36fd3726cef581b8246a0072aafb19f78a4fde8b
Chubrel <maxim.potapov2002@gmail.com>
2020-10-01 06:24:55 +0300

    Done lab2 and lab3

---
+++ Homework/Task 6 p 36/Task 6 p 36/Task 6 p 36.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x, y, min, max;
+    setlocale(0, "");
+    cout << "Введите числа x и y.\n";
+    cin >> x >> y;
+
+    if (x > y) {
+        min = y;
+        max = x;
+    }
+    else {
+        min = x;
+        max = y;
+    }
+
+    double z = (min + 0.5) / (1 + max * max);
+    cout << "Полученное число Z = " << z;
+}
---
+++ Homework/Task 8 p 36/Task 8 p 36/Task 8 p 36.cpp
@@ -0,0 +1,23 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x, y, r, fi;
+    setlocale(0, "");
+    cout << "Введите координаты x и y.\n";
+    cin >> x >> y;
+    r = sqrt(x * x + y * y);
+    if (x > 0)
+        if (y >= 0) fi = atan(y / x);
+        else fi = 2 * M_PI + atan(y / x);
+    else if (x == 0)
+        if (y > 0) fi = M_PI / 2;
+        else if (y < 0) fi = 1.5 * M_PI;
+        else fi = 0;
+    else fi = M_PI + atan(y / x);
+
+    cout << "Полярные координаты точки: (" << r << ", " << fi << ")";
+}
